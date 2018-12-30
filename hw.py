c2971afed362d0bfdb595784d67fe34a2b972ea4
robert1003 <b07902047@ntu.edu.tw>
2018-12-30 23:19:57 +0800

    YA

---
+++ hw.py
@@ -0,0 +1,85 @@
+# Imports here
+import torch
+from torch import nn, optim
+import torch.nn.functional as F
+import numpy as np
+from torchvision import datasets, transforms, models
+from torch.utils.data.sampler import SubsetRandomSampler
+
+data_dir = './flower_data'
+train_dir = data_dir + '/train'
+valid_dir = data_dir + '/valid'
+
+# TODO: Define your transforms for the training and validation sets
+data_transforms = transforms.Compose([
+    transforms.RandomRotation(30),
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# TODO: Load the datasets with ImageFolder
+train_data = datasets.ImageFolder(train_dir, transform=data_transforms)
+valid_data = datasets.ImageFolder(valid_dir, transform=data_transforms)
+
+# TODO: Using the image datasets and the trainforms, define the dataloaders
+trainloader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True)
+validloader = torch.utils.data.DataLoader(valid_data, batch_size=32, shuffle=True)
+
+import json
+
+with open('cat_to_name.json', 'r') as f:
+    cat_to_name = json.load(f)
+
+# TODO: Build and train your network
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = models.vgg19(pretrained=True)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.classifier = nn.Sequential(nn.Linear(1024, 512),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(512, 256),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.2),
+                                 nn.Linear(256, 102),
+                                 nn.LogSoftmax(dim=1))
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), lr=0.01)
+model.to(device)
+
+epochs = 30
+min_valid_loss = np.Inf
+for e in range(epochs):
+    trainloss = 0
+    for inputs, labels in trainloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+        loss.backward()
+        optimizer.step()
+
+        trainloss += loss.item()
+
+    validloss = 0
+    for inputs, labels in validloader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        logps = model.forward(inputs)
+        loss = criterion(logps, labels)
+
+        validloss += loss.item()
+
+    if validloss < min_valid_loss:
+        torch.save(model.state_dict(), 'checkpoint.pt')
+        min_valid_loss = validloss
+
+    print(f'trainloss: {trainloss/len(trainloader)}, testloss: {testloss/len(testloader)}')
+
